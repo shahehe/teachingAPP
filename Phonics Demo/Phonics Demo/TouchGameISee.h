ab7a1d97be0021aac33e3548fed49fa96e25d1f9
yiplee <guoyinl@gmail.com>
2014-03-18 22:38:40 +0800

    touch game : I see

---
+++ Phonics Demo/Phonics Demo/TouchGameISee.h
@@ -0,0 +1,13 @@
+//
+//  TouchGameISee.h
+//  LetterE
+//
+//  Created by yiplee on 14-3-18.
+//  Copyright (c) 2014年 USTB. All rights reserved.
+//
+
+#import "TouchGameLayer.h"
+
+@interface TouchGameISee : TouchGameLayer
+
+@end
---
+++ Phonics Demo/Phonics Demo/TouchGameISee.m
@@ -0,0 +1,13 @@
+//
+//  TouchGameISee.m
+//  LetterE
+//
+//  Created by yiplee on 14-3-18.
+//  Copyright (c) 2014年 USTB. All rights reserved.
+//
+
+#import "TouchGameISee.h"
+
+@implementation TouchGameISee
+
+@end
