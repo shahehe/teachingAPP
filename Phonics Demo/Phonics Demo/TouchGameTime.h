9bb0178fdd85861957d9266593b86ad0d72587fa
yiplee <guoyinl@gmail.com>
2014-03-21 16:24:54 +0800

    Xcode file

---
+++ Phonics Demo/Phonics Demo/TouchGameTime.h
@@ -0,0 +1,13 @@
+//
+//  TouchGameTime.h
+//  LetterE
+//
+//  Created by yiplee on 14-3-21.
+//  Copyright (c) 2014年 USTB. All rights reserved.
+//
+
+#import "TouchGameLayer.h"
+
+@interface TouchGameTime : TouchGameLayer
+
+@end
---
+++ Phonics Demo/Phonics Demo/TouchGameTime.m
@@ -0,0 +1,13 @@
+//
+//  TouchGameTime.m
+//  LetterE
+//
+//  Created by yiplee on 14-3-21.
+//  Copyright (c) 2014年 USTB. All rights reserved.
+//
+
+#import "TouchGameTime.h"
+
+@implementation TouchGameTime
+
+@end
