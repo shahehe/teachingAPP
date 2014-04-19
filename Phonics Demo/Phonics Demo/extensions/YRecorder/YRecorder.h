9392f16d7f0dc258171f69d208bfe7716ae2c891
shahehe <shahehe@gmail.com>
2014-04-18 22:54:58 -0700

    add recorder
    1. #import "YRecorder.h"
    
    2. usage
    start recording: [[YRecorder sharedEngine] record];
    stop recording:  [[YRecorder sharedEngine] stop];
    play recorded voice:     [[YRecorder sharedEngine] play];

---
+++ Phonics Demo/Phonics Demo/extensions/YRecorder/YRecorder.h
@@ -0,0 +1,23 @@
+//
+//  YRecorder.h
+//  LetterB
+//
+//  Created by Yan Feng on 3/22/14.
+//  Copyright (c) 2014 USTB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+@interface YRecorder : NSObject <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
++ (YRecorder *) sharedEngine;
+-(void) record;
+-(void) play;
+-(void) stop;
+-(NSString *) getWaveFileName;
+-(void) setDelegate:(id)delegate;
+@property (nonatomic, strong) NSString * currWav;
+@property (nonatomic,strong)  AVAudioRecorder *recorder;
+@property (nonatomic, strong) AVAudioPlayer *player;
+@property (nonatomic, strong) AVAudioSession * session;
+
+@end
---
+++ Phonics Demo/Phonics Demo/extensions/YRecorder/YRecorder.m
@@ -0,0 +1,126 @@
+//
+//  YRecorder.m
+//  LetterB
+//
+//  Created by Yan Feng on 3/22/14.
+//  Copyright (c) 2014 USTB. All rights reserved.
+//
+
+#import "YRecorder.h"
+
+@implementation YRecorder
+@synthesize currWav;
+@synthesize recorder;
+@synthesize player;
+@synthesize session;
+static YRecorder * engine = nil;
+-(void) setDelegate:(id)delegate
+{
+    [player setDelegate:delegate];
+}
+NSMutableDictionary *recordSetting;
+- (id)init
+{
+    if (self = [super init])
+    {
+        // Initialization code here.
+        session = [AVAudioSession sharedInstance];
+        [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+        UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_Speaker;
+        AudioSessionSetProperty (kAudioSessionProperty_OverrideAudioRoute,sizeof (audioRouteOverride),&audioRouteOverride);
+        // Define the recorder setting
+        recordSetting = [[NSMutableDictionary alloc] init];
+        
+        [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+        [recordSetting setValue:[NSNumber numberWithFloat:16000] forKey:AVSampleRateKey];
+        [recordSetting setValue:[NSNumber numberWithInt: 1] forKey:AVNumberOfChannelsKey];
+        //  [recordSetting setValue:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+        
+        recorder = nil;
+        
+
+    }
+    return self;
+}
+
++(YRecorder *)sharedEngine
+{
+    if ( engine == nil)
+    {
+          engine = [[super allocWithZone:NULL] init];
+        
+    }
+    return engine;
+}
+-(void) record
+{
+    if ( recorder )
+    {
+        [recorder stop];
+        [recorder release];
+        recorder = nil;
+    }
+    // Initiate and prepare the recorder
+    NSString *documentDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    
+    NSString * timeStampValue = [NSString stringWithFormat:@"%ld", (long)[[NSDate date] timeIntervalSince1970]];
+    
+    NSURL *audioURL = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/%@.wav",documentDirectory,timeStampValue]];
+    
+    
+    currWav = [[NSString alloc] initWithFormat:@"%@/%@.wav",documentDirectory,timeStampValue];
+    NSError * error = nil;
+
+    recorder = [[AVAudioRecorder alloc] initWithURL:audioURL settings:recordSetting error:&error];
+    
+    
+    
+    recorder.meteringEnabled = YES;
+    [recorder prepareToRecord];
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+    [recorder record];
+}
+-(NSString *) getWaveFileName
+{
+    return currWav;
+}
+-(void) play
+{
+    [player stop];
+    [player setCurrentTime:0.0];
+    [player release];
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+    //[player setDelegate:self];
+
+    [player play];
+}
+-(void) stop
+{
+    [recorder stop];
+}
+// singleton methods
++ (id)allocWithZone:(NSZone *)zone {
+    return [[self sharedEngine] retain];
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+- (id)retain {
+    return self;
+}
+
+- (NSUInteger)retainCount {
+    return NSUIntegerMax;  // denotes an object that cannot be released
+}
+
+
+- (id)autorelease {
+    return self;
+}
+
+-(void)dealloc {
+    [super dealloc];
+}
+@end
